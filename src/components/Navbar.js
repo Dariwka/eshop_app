ab12775cbddd7b7c0bf08e72d4a972d36ad7075f
Dariwka <77488950+Dariwka@users.noreply.github.com>
2021-09-30 13:15:30 +0300

    add Jenkinsfile script

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,64 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import "../scss/navBar.scss";
+import { Link } from "react-router-dom";
+
+// import "../scss/navBar.scss";
+
+// const Navbar = () => {
+//   const location = useLocation();
+//   const { pathname } = location;
+//   const splitLocation = pathname.split("/");
+//   return (
+//     <nav className="nav-wrapper">
+//       <div className="container">
+//         <Link to="/" className="brand-logo">
+//           E-Shop
+//         </Link>
+//         <div className="menu-bar">
+//           <ul>
+//             <li className={splitLocation[1] === "" ? "active" : ""}>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li className={splitLocation[1] === "products" ? "active" : ""}>
+//               <Link to="/products">Products</Link>
+//             </li>
+//             <li className={splitLocation[1] === "cart" ? "active" : ""}>
+//               <Link to="/cart">
+//                 <ShoppingCartIcon />
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
 
 const Navbar = () => {
-  const location = useLocation();
-  const { pathname } = location;
-  const splitLocation = pathname.split("/");
   return (
     <nav className="nav-wrapper">
       <div className="container">
         <Link to="/" className="brand-logo">
-          E-Shop
+          E-shop
         </Link>
-        <div className="menu-bar">
-          <ul>
-            <li className={splitLocation[1] === "" ? "active" : ""}>
-              <Link to="/">Home</Link>
-            </li>
-            <li className={splitLocation[1] === "products" ? "active" : ""}>
-              <Link to="/products">Products</Link>
-            </li>
-            <li className={splitLocation[1] === "cart" ? "active" : ""}>
-              <Link to="/cart">
-                <ShoppingCartIcon />
-              </Link>
-            </li>
-          </ul>
-        </div>
+
+        <ul className="right">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Shop</Link>
+          </li>
+          <li>
+            <Link to="/cart">My cart</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <i className="material-icons">shopping_cart</i>
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 };
-
 export default Navbar;
